eb25a95dcbdb0ef8f598ff84cbeb35490462386b
sammed.limbikai <sammed.limbikai@cmssl.in>
2024-09-25 14:43:04 +0530

    feat(dist unzip): dist unzipped successfully

--- lib/firebase/fire_storage.dart
+++ lib/firebase/fire_storage.dart
@@ -4,6 +4,7 @@ import 'package:archive/archive.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 class FireBaseStorage {
   static late final FirebaseStorage _storage;
@@ -19,18 +20,22 @@ class FireBaseStorage {
           .ref()
           .child('angular_distributions/dist_uat_21sep2024_2039pm.zip');
       Directory cacheDir = await getTemporaryDirectory();
+      log.log(Level.info, "cache dir = ${cacheDir.path}");
       File zipFile = File('${cacheDir.path}/angular_dist.zip');
       await fileRef.writeToFile(zipFile);
 
-      // List<int> bytes = zipFile.readAsBytesSync();
-      // Archive archive = ZipDecoder().decodeBytes(bytes);
-      // for (var file in archive) {
-      //   if (file.isFile) {
-      //     String filePath = '${cacheDir.path}/${file.name}';
-      //     File extractedFile = File(filePath);
-      //     await extractedFile.writeAsBytes(file.content);
-      //   }
-      // }
+      List<int> bytes = zipFile.readAsBytesSync();
+      Archive archive = ZipDecoder().decodeBytes(bytes);
+      for (var file in archive) {
+        if (file.isFile) {
+          String extractedFilePath = p.join(cacheDir.path, file.name);
+          await Directory(p.dirname(extractedFilePath)).create(recursive: true);
+          File extractedFile = File(extractedFilePath);
+          await extractedFile.writeAsBytes(file.content);
+        }
+      }
+
+      log.log(Level.info, "Unzipping completed successfully!");
     } catch (e) {
       log.log(Level.info, "Error geting angular dist : ${e.toString()}");
     }
